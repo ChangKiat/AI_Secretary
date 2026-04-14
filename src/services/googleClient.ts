3f90d484cfe25e98cc324285c0d33c980bb57f16
ChangKiat <changkiat1995@gmail.com>
2026-04-14 22:51:06 +0800

    Using  GOOGLE_CREDENTIALS_JSON in .env file if deploy on cloud

--- src/services/googleClient.ts
+++ src/services/googleClient.ts
@@ -1,9 +1,29 @@
 import { google } from 'googleapis';
 
-export const googleAuth = new google.auth.GoogleAuth({
-    keyFile: './google-credentials.json', 
-    scopes: [
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/calendar'
-    ],
-});
+const scopes = [
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/calendar'
+];
+
+let auth;
+
+if (process.env.GOOGLE_CREDENTIALS_JSON) {
+    console.log("☁️ Running on Cloud: Using injected Google Credentials");
+    
+    const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
+    
+    auth = new google.auth.GoogleAuth({
+        credentials, 
+        scopes,
+    });
+} 
+else {
+    console.log("💻 Running Locally: Using google-credentials.json file");
+    
+    auth = new google.auth.GoogleAuth({
+        keyFile: './google-credentials.json',
+        scopes,
+    });
+}
+
+export const googleAuth = auth;
